9ff2d9bdc38027317539de15b4f38b65f506b07b
Torrinos <ole.bjorne@gmail.com>
2016-05-22 21:46:12 -0500

    database and scraper of team stats

--- scraper_team_stats.py
+++ scraper_team_stats.py
@@ -1,10 +1,14 @@
+# This file download the summary of the whole season from the following link:
+# http://www.nhl.com/stats/rest/grouped/teams/game/teamsummary?cayenneExp=seasonId=20152016%20and%20gameTypeId=2
+# It does not require the team names, just the season ID and game type ID
+
 import json
 import requests
 import pandas as pd
 import time
 from sqlalchemy import create_engine
 
-#initializing
+#preparing headers
 hdrs = {
   'Host': 'www.nhl.com',
   'Connection': 'keep-alive',
@@ -14,53 +18,53 @@ hdrs = {
   'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
   }
 
-#requests
-with open('requests_teams.lst') as f:
-    reqs = f.readlines()
-
-#postgres
+#preparing postgres engine
 engine = create_engine('postgresql://postgres:e2p71828@localhost:5432/nhl')
 conn = engine.connect()
 
-def get_df(reqtype,season,goalie):
-    print 'Parsing: '+reqtype
+
+#get team summary by season by game type
+def get_team_summary_by_season(season,gameTypeId):
+    print 'Parsing: '+str(season)
     
-    url = 'http://www.nhl.com/stats/rest/grouped/teams/'
-    url = url + reqtype
-    url = url + '?cayenneExp=seasonId='+season+'%20and%20gameTypeId=2'
+    url = 'http://www.nhl.com/stats/rest/grouped/teams/game/'
+    url = url + 'teamsummary?cayenneExp=seasonId='
+    url = url+season+'%20and%20gameTypeId='+gameTypeId
     
     res = requests.get(url, headers = hdrs)
     data = json.loads(res.content)
-    # return pd.DataFrame(data['data']).set_index('teamId')
-    return pd.DataFrame(data['data'])
-
-#get full dataset for a given season
-def get_full_set(seasonId):
-    teams = {}
-    #get teams
-    for req in reqs:
-        url = req.rstrip()
-        keys = url.split('/')
-        key = keys[len(keys)-1]
-        teams[key] = get_df(url,seasonId,False)
-        time.sleep(10)
-    
-        final = pd.DataFrame()
-        #combined dataframe for teams
-        for key in teams:
-            if final.empty:
-                final = teams[key]
-            else:
-                df = teams[key]            
-                cols_to_use = df.columns.difference(final.columns) 
-                final = pd.merge(final, df[cols_to_use], left_index=True, 
-                                 right_index=True, how='outer')
-    return final
+    return pd.DataFrame(data['data']).set_index(['gameId'])
 
 
 if __name__ == '__main__':
-    season = '20092010'
-    teams = get_full_set(season)
+    seasonId = ['20062007','20072008','20082009','20092010','20102011',\
+    '20112012','20122013','20132014','20142015']
 
-    #pickle teams
-    teams.to_sql('teams',engine,if_exists='append')
+#SCRAPER PART
+    # mapping first season
+    # df = pd.concat([get_team_summary_by_season('20052006','2'),get_team_summary_by_season('20052006','3')])
+
+    # iterate over seasons
+    # for season in seasonId:
+    #   time.sleep(10)
+    #   df = pd.concat([df,get_team_summary_by_season(season,'2')])
+    #   time.sleep(10)
+    #   df = pd.concat([df,get_team_summary_by_season(season,'3')])
+
+    # scrapping last season
+    # df = pd.concat([df,get_team_summary_by_season('20152016','2')])
+    # df = pd.concat([df,get_team_summary_by_season('20152016','3')])
+
+    # writing to database
+    # df.to_sql('team_stats_by_game',engine,if_exists='append')
+
+#PROCESSING PART
+    # read existing dataframe
+    df = pd.read_sql('team_stats_by_game',engine)
+
+    # add season ID and gameTypeId
+    df['seasonId'] = df.apply(lambda x: int(str(x.gameId)[0:4]),axis=1)
+    df['gameTypeId'] = df.apply(lambda x: int(str(x.gameId)[5:6]),axis=1)
+
+    # rewrite the dataframe
+    df.to_sql('team_stats_by_game',engine,if_exists='replace')
